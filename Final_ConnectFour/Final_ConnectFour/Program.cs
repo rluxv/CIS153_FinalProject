51b78539332dd8f9647e8fc82e485c7b7c9e0934
Nicholas F <nfrederick01@protonmail.com>
2023-03-28 20:22:04 -0400

    N.F. - Make program DPI aware - prevents program from being blurry on scaled displays

--- Final_ConnectFour/Final_ConnectFour/Program.cs
+++ Final_ConnectFour/Final_ConnectFour/Program.cs
@@ -14,9 +14,17 @@ namespace Final_ConnectFour
         [STAThread]
         static void Main()
         {
+            // If we are using vista or newer, we can make it so the program is not blurry when using scaled displays.
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainMenu());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
