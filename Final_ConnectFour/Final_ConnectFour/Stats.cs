ccc4a9b818b3d248b1210f6b4234e6057a21e244
Nicholas F <nfrederick01@protonmail.com>
2023-04-05 10:03:11 -0400

    Nicholas - Update Stats.cs

--- Final_ConnectFour/Final_ConnectFour/Stats.cs
+++ Final_ConnectFour/Final_ConnectFour/Stats.cs
@@ -8,5 +8,36 @@ namespace Final_ConnectFour
 {
     internal class Stats
     {
+        enum STAT_TYPES
+        {
+            PLAYERWINCOUNT,
+            COMPUTERWINCOUNT,
+            GAMETIECOUNT,
+            GAMESPLAYEDCOUNT,
+            PLAYERWINPERCENTAGE,
+            COMPUTERWINPERCENTAGE
+        }
+        Stats()
+        {
+
+        }
+
+        private int playerWinCount, computerWinCount, gameTieCount, gamesPlayedCount, playerWinPercentage, computerWinPercentage;
+
+        public void loadStatsFromFile()
+        {
+
+        }
+
+        public void saveStat()
+        {
+
+        }
+
+        public int getStat(int STAT_TYPES)
+        {
+
+            return 0;
+        }
     }
 }
