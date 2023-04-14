e45ea6c952351de8157c78e53ba3798748036b2a
Nicholas F <nfrederick01@protonmail.com>
2023-04-14 12:31:42 -0400

    Added win check function - only works with vertical check atm

--- Final_ConnectFour/Final_ConnectFour/Board_2Player.cs
+++ Final_ConnectFour/Final_ConnectFour/Board_2Player.cs
@@ -61,6 +61,38 @@ namespace Final_ConnectFour
             lbl_turnNum.Text = "Turn " + turns;
         }
 
+        private bool checkWin(int p)
+        {
+            // vertical check
+            for(int c = 0; c < 7; c++)
+            {
+                int count = 0;
+                for(int r = 0; r < 6; r++)
+                {
+                    if(board.getCell(c, r).getToken() == p)
+                    {
+                        Console.WriteLine("token found: " + c + ", " + r);
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+                if(count == 4)
+                {
+                    return true;
+                }
+            }
+
+            // vertical check
+
+            // diagonal check
+
+
+            return false;
+        }
+
         private void btn_click(object sender, EventArgs e)
         {
             if (sender is Button)
@@ -138,6 +170,14 @@ namespace Final_ConnectFour
                 }
 
                 refreshSidebar();
+                if(checkWin(1))
+                {
+                    Console.WriteLine("player 1 winner");
+                }
+                else if(checkWin(2))
+                {
+                    Console.WriteLine("player 2 winner");
+                }
 
             }
         }
